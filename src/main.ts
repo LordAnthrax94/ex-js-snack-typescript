2e17ba476379eb6f68069c4d67df5bd6e914a7b6
LordAnthrax94 <andreagi1994@gmail.com>
2025-06-13 16:38:44 +0200

    Feat: completato snack 3

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,20 @@ type Dipendente = {
   annoNascita: number,
   sesso: "M" | "F", // solo M o F
   anniDiServizio: number[]
+}
 
+// Snack 3
+
+type Developer = Dipendente &{
+  livelloEsperienza: "Junior" | "Mid" | "Senior",
+  linguaggi?: string[], // opzionale
+  certificazioni: string[] // può essere vuoto
+}
+
+type ProjectManager = Dipendente & {
+  teamSize: number | null, // può essere null se non ha ancora un team assegnato
+  budgetGestito?: number, // opzionale
+  stakeholderPrincipali: string[] // può essere vuoto
 }
 
 
@@ -29,16 +42,6 @@ type Dipendente = {
 
 
 
-// Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
-
-// Developer
-// livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
-// linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
-// certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
-// ProjectManager
-// teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
-// budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
-// stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
   
   
 
