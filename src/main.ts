e1133c17571826fa31008e1a73f53c241d7d41fb
LordAnthrax94 <andreagi1994@gmail.com>
2025-06-13 16:31:25 +0200

    Feat: completato snack 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
+let dato: unknown;
+
+if(typeof dato === "string"){
+  console.log(dato.toUpperCase());   
+}else if (typeof dato === "number"){
+  console.log(dato * 2);  
+}else if (typeof dato === "boolean"){
+  console.log(dato ? "Si" : "No");  
+}else{
+  console.log("Tipo non supportato");
+}
+  
+  
 
