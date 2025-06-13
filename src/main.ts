59d0a595f81fd835c42ce79ffacfad63c962e94d
LordAnthrax94 <andreagi1994@gmail.com>
2025-06-13 16:36:37 +0200

    Feat: completato snask 2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// Snack 1
+
 let dato: unknown;
 
 if(typeof dato === "string"){
@@ -9,6 +11,34 @@ if(typeof dato === "string"){
 }else{
   console.log("Tipo non supportato");
 }
+
+//Snack 2
+
+type Dipendente = {
+  nome: string,
+  cognome: string,
+  annoNascita: number,
+  sesso: "M" | "F", // solo M o F
+  anniDiServizio: number[]
+
+}
+
+
+
+
+
+
+
+// Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+
+// Developer
+// livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+// linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+// certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+// ProjectManager
+// teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+// budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+// stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
   
   
 
